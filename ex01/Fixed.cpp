f297095a24290f2596ee08c89e03a2c705b35bda
Enes Bakıroğlu <ensbakirog@gmail.com>
2024-09-01 22:42:15 +0300

    ex01 done

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,6 +6,24 @@ Fixed::Fixed(){
 	this->_fixedPointValue = 0;
 }
 
+Fixed::Fixed( const int number ) {
+	std::cout << "Int constructor called" << std::endl;
+	this->_fixedPointValue = (number << this->_fractionalBits);
+}
+
+int Fixed::toInt( void ) const {
+	return this->_fixedPointValue / (1 << this->_fractionalBits);
+}
+
+Fixed::Fixed( const float number ) {
+	std::cout << "Float constructor called" << std::endl;
+	this->_fixedPointValue = roundf(number * (1 << this->_fractionalBits));
+}
+
+float Fixed::toFloat( void ) const {
+	return (float)(this->_fixedPointValue / (float)(1 << this->_fractionalBits));
+}
+
 Fixed::Fixed( const Fixed& obj ) {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = obj;
@@ -15,7 +33,7 @@ Fixed& Fixed::operator=( const Fixed& obj ) {
 	std::cout << "Copy assignment operator called" << std::endl;
 
 	if (this != &obj)
-		this->_fixedPointValue = obj.getRawBits();
+		this->_fixedPointValue = obj._fixedPointValue;
 	return *this;
 }
 
@@ -31,4 +49,9 @@ int Fixed::getRawBits( void ) const {
 void Fixed::setRawBits( int const raw ) {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->_fixedPointValue = raw;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& _fixed) {
+	os << _fixed.toFloat();
+	return os;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 class Fixed
 {
 	private:
@@ -7,10 +9,17 @@ class Fixed
 		static const int _fractionalBits = 8; 
 	public:
 		Fixed();
+		Fixed( int number );
+		Fixed( const float number );
 		Fixed( const Fixed& obj );
 		Fixed& operator=( const Fixed& obj );
 		~Fixed();
 
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
+
+		int toInt( void ) const;
+		float toFloat( void ) const;
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& _fixed);
